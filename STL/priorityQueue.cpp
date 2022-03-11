c597f04b4bb93cdfcbcfa282647693c379cb6dc4
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-03-11 15:42:48 +0530

    Create priorityQueue.cpp

---
+++ STL/priorityQueue.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void PriorityQueue(priority_queue<int> pq){
+    priority_queue<int> q = pq;
+    while(!q.empty()){
+        cout<<q.top()<<" ";
+        q.pop();
+    }
+    cout<<endl;
+}
+
+int main(){
+    priority_queue<int> q;
+    q.push(10);
+    q.push(-3);
+    q.push(7);
+    q.push(8);
+    
+    PriorityQueue(q);
+    cout<<q.size()<<endl;
+    
+    q.pop();
+    q.pop();
+    cout<<q.empty()<<endl;
+    PriorityQueue(q);
+
+}
